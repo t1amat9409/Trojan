10cd33aa536a4f87fb0a1403384bf9d720c74b3f
CodeZAR <9409promos@gmail.com>
2017-07-12 16:14:47 +0200

    Update trojan.js

--- dist/trojan.js
+++ dist/trojan.js
@@ -295,3 +295,177 @@ Trojan.style.prototype = {
 		return this;
 	}
 };
+
+Trojan.Intro = function(){
+	this.style = new Trojan.style("styleID")
+	var styl = {
+	  ".over-lay":{
+	    position:"fixed",
+	    left:0,
+	    right:0,
+	    top:0,
+	    bottom:0,
+	    background:"rgba(0,0,0,0.7)"
+	  },
+	  ".over-lay-content":{
+	    "background":"none",
+	    "position":"absolute",
+	    "box-sizing": "border-box",
+	    "border-radius": "5px",
+	    "border": "2px solid #4ac"
+	  },
+	  ".over-lay-controls":{
+	    "background":"#fff",
+	    position:"absolute",
+	    "box-sizing": "border-box",
+	    padding: "10px",
+	    "border-radius": "5px",
+	    border: "2px solid #4ac"
+	  },
+	  ".over-lay-controls-context":{
+	    padding: "5px 10px",
+	  },
+	  ".over-lay-controls-buttons":{
+	    display:"flex",
+	    "align-items":"center"
+	  },
+	  ".over-lay-controls-buttons > button":{
+	    padding: "5px 10px",
+	    border: "none",
+	    margin: "5px"
+	  }
+	}
+	this.style.declare(styl).run()
+};
+
+Trojan.Intro.prototype = {
+  constructor:Trojan.Intro,
+  targets:[],
+  current:0,
+  elem:document.createElement("div"),
+  add:function(target,text){
+    const _that = this
+    const elem = {
+      target:target,
+      width:target.offsetWidth,
+      height:target.offsetHeight+4,
+      top:target.offsetTop+20,
+      left:target.offsetLeft+8,
+      text:text
+    }
+    
+    this.targets.push(elem)
+    
+    return this
+  },
+  run:function(n){
+    const _that = this
+    
+    
+    _that.elem.className = "over-lay"
+    _that.elem.innerHTML = ""
+    let bool = Boolean(n)
+    let pos = n
+    //console.log(exist)
+    const div1 = document.createElement("div");
+    div1.className = "over-lay-content"
+    const div2 = document.createElement("div");
+    div2.className = "over-lay-controls"
+    const context = document.createElement("div")
+    context.className = "over-lay-controls-context"
+    
+    const buttons = document.createElement("div")
+    buttons.className = "over-lay-controls-buttons"
+    
+    const btn1 = document.createElement("button")
+    const btn2 = document.createElement("button")
+    const btn3 = document.createElement("button")
+    
+    if(pos>(_that.targets.length-1)){
+       btn1.setAttribute("disabled",true)
+    }
+   
+    btn1.innerText = "Done"
+    btn1.onclick = function(){
+      const el = document.querySelectorAll(".over-lay")
+      el.forEach((item,i)=>{
+        item.parentNode.removeChild(item)
+      })
+    }
+    
+    if((pos===0)){
+       btn2.setAttribute("disabled",true)
+    }
+    
+    btn2.innerText = "Prev"
+    btn2.onclick = function(){
+      const el = document.querySelectorAll(".over-lay")
+      el.forEach((item,i)=>{
+        item.parentNode.removeChild(item)
+      })
+      
+      _that.run(n-1)
+    }
+    
+    if(pos===(_that.targets.length-1)){
+       btn3.setAttribute("disabled",true)
+    }
+    
+    btn3.innerText = "Next"
+    btn3.onclick = function(){
+      const el = document.querySelectorAll(".over-lay")
+      el.forEach((item,i)=>{
+        item.parentNode.removeChild(item)
+      })
+      _that.run(n+1)
+    }
+    
+    buttons.appendChild(btn1)
+    buttons.appendChild(btn2)
+    buttons.appendChild(btn3)
+    
+    if(n!==0){
+      _that.current = n
+      const content = _that.targets[n]
+      const cln = content.target.cloneNode(true)
+      div1.appendChild(cln)
+      div1.style.top = content.top+"px"
+      div1.style.left = content.left+"px"
+      div1.style.width = content.width+"px"
+      div1.style.height = content.height+"px"
+      
+      div2.style.left = content.left+"px"
+      div2.style.top = (content.height+content.top+10)+"px"
+      div2.style.maxWidth = content.width+"px"
+      
+      context.innerHTML = content.text
+      div2.appendChild(context)
+      div2.appendChild(buttons)
+      
+      _that.elem.appendChild(div1)
+      _that.elem.appendChild(div2)
+      document.body.append(_that.elem)
+    } else{
+      const content = _that.targets[0]
+      const cln = content.target.cloneNode(true)
+      div1.appendChild(cln)
+      div1.style.top = content.top+"px"
+      div1.style.left = content.left+"px"
+      div1.style.width = content.width+"px"
+      div1.style.height = content.height+"px"
+      
+      div2.style.left = content.left+"px"
+      div2.style.top = (content.height+content.top+10)+"px"
+      div2.style.maxWidth = content.width+"px"
+      
+      
+      context.innerHTML = content.text
+      div2.appendChild(context)
+      div2.appendChild(buttons)
+      
+      _that.elem.appendChild(div1)
+      _that.elem.appendChild(div2)
+      document.body.append(_that.elem)
+    }
+  }
+}
